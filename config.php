fdcae65019e7ffd6c248acca33273ef11c1f23cc
mams7 <45550217+mams7@users.noreply.github.com>
2018-12-03 11:37:32 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+//ikuti format config di bawah "ticket=tiket_kamu&meid=meid_kamu",
+//jangan ada sepasi
+$config = [
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBh4OZrqqSor-1c869oIOifnaq24OUhpi_iIKakaOwz3-Hod2wvY6ktM52kq2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=942472c17507",
+  ];
+
+?>
