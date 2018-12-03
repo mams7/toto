943c3c23dd9ae800e44f81a1e014e10688f01c14
mams7 <45550217+mams7@users.noreply.github.com>
2018-12-03 12:20:20 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,9 @@
 <?php
 //ikuti format config di bawah "ticket=tiket_kamu&meid=meid_kamu",
-//jangan ada sepasi
+//jangan ada sepasi ya bro
+//by Mams7
 $config = [
-"ticket=sHp2r312otmCpnGns3mCqJhovNiBh4OZrqqSor-1c869oIOifnaq24OUhpi_iIKakaOwz3-Hod2wvY6ktM52kq2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=942472c17507",
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBh4vcutCSobOlmcu8ipmgiomu3ILMcWS1iIVjhbDN3YqtiM66uoKks6WGla2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=e9fb5bc91fd09a17",
   ];
 
 ?>
